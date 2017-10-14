168893596003fbd37cd32f8824179afb7015ea48
wgarcia <android@cuc.edu.co>
2017-10-14 09:10:26 -0500

    Formulario crear persona completo

--- app/src/main/java/co/jamesfl/apps/personasmaterial/CrearPersona.java
+++ app/src/main/java/co/jamesfl/apps/personasmaterial/CrearPersona.java
@@ -7,15 +7,20 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class CrearPersona extends AppCompatActivity {
 
-    private EditText cajaNombre, cajaApellido;
-    private TextInputLayout icajaNombre, icajaApellido;
+    private EditText cajaCedula, cajaNombre, cajaApellido;
+    private TextInputLayout icajaCedula, icajaNombre, icajaApellido;
     private ArrayList<Integer> fotos;
+    private String[] sexo_;
+    private Spinner spnsexo;
+    private ArrayAdapter<String> adapter;
     private Resources res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +28,40 @@ public class CrearPersona extends AppCompatActivity {
         setContentView(R.layout.activity_crear_persona);
 
         res = this.getResources();
+
+
+        cajaCedula = (EditText)findViewById(R.id.txtCedula);
+        cajaNombre = (EditText)findViewById(R.id.txtNombre);
+        cajaApellido = (EditText)findViewById(R.id.txtApellido);
+
+        icajaCedula = (TextInputLayout)findViewById(R.id.cajaCedula);
+        icajaNombre = (TextInputLayout)findViewById(R.id.cajaNombre);
+        icajaApellido = (TextInputLayout)findViewById(R.id.cajaApellido);
+
+        spnsexo = (Spinner)findViewById(R.id.spnSexo);
+        sexo_ = res.getStringArray(R.array.opc_sexo);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sexo_);
+        spnsexo.setAdapter(adapter);
+
+        iniciarFotos();
+    }
+
+    public void iniciarFotos(){
         fotos = new ArrayList<>();
         fotos.add(R.drawable.carro1);
         fotos.add(R.drawable.carro2);
         fotos.add(R.drawable.carro3);
         fotos.add(R.drawable.carro4);
-
-        cajaNombre = (EditText)findViewById(R.id.txtNombre);
-        cajaApellido = (EditText)findViewById(R.id.txtApellido);
-
-        icajaNombre = (TextInputLayout)findViewById(R.id.cajaNombre);
-        icajaApellido = (TextInputLayout)findViewById(R.id.cajaApellido);
     }
 
     public void guardar(View v){
-        String nombre, apellido;
+        String cedula, nombre, apellido;
+        int sexo;
+        cedula = cajaCedula.getText().toString();
         nombre = cajaNombre.getText().toString();
         apellido = cajaApellido.getText().toString();
-        Persona p = new Persona(metodos.fotoAleatoria(fotos), nombre, apellido);
+        sexo = spnsexo.getSelectedItemPosition();
+        Persona p = new Persona(metodos.fotoAleatoria(fotos), cedula, nombre, apellido, sexo);
         p.guardar();
         Snackbar.make(v, res.getString(R.string.msjguardado), Snackbar.LENGTH_SHORT).setAction("Action", null).show();
         limpiar();
